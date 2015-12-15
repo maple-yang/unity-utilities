8143b3a0c077661d7a48e1f35c4ce0d8ee84b727
William Towns <wtowns@gmail.com>
2015-12-14 17:06:01 -0800

    Add MultiCoroutine class

---
+++ MultiCoroutine.cs
@@ -0,0 +1,51 @@
+﻿// MultiCoroutine.cs
+// Copyright (c) 2015 Orbital Games, LLC.
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace OrbitalGames.UnityUtilities
+{
+	public class MultiCoroutine
+	{
+		private ICollection<IEnumerator> _coroutines;
+		private MonoBehaviour _host;
+		private int _remaining;
+
+		public MultiCoroutine(ICollection<IEnumerator> coroutines, MonoBehaviour host)
+		{
+			_coroutines = coroutines;
+			_host = host;
+		}
+
+		public static IEnumerator YieldMultiple(ICollection<IEnumerator> coroutines, MonoBehaviour host)
+		{
+			return (new MultiCoroutine(coroutines, host)).StartAll();
+		}
+
+		public IEnumerator StartAll()
+		{
+			if (_remaining > 0)
+			{
+				yield break;
+			}
+			_remaining = _coroutines.Count;
+			foreach (var coroutine in _coroutines)
+			{
+				_host.StartCoroutine(StartSingle(coroutine));
+			}
+			while (_remaining > 0)
+			{
+				yield return 0;
+			}
+			yield break;
+		}
+
+		private IEnumerator StartSingle(IEnumerator coroutine)
+		{
+			yield return _host.StartCoroutine(coroutine);
+			--_remaining;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * ColorUtil: Utilities for working with the Color object.
 * GameObjectUtils: Utilities for working with GameObjects.
 * TransformUtils: Utilities for working with Transforms.
+* MultiCoroutine: Class for running multiple simultaneous coroutines and yielding until all are finished.
